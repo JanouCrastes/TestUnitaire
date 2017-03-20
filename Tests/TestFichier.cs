77bc860840e96d39febfc2c9316902e0848fb959
JanouCrastes <janou.crastes@gmail.com>
2017-03-20 18:49:44 +0100

    insertionTestUnitaire

---
+++ Tests/TestFichier.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSystem.Tests
+{
+    [TestClass]
+    public class TestFichier
+    {
+
+        [TestMethod]
+        public void TestCanWriteOk()
+        {
+            Fichier f = new Fichier("Test", null);
+            f.chmod(6);
+            Assert.AreEqual(true, f.canWrite());
+        }
+
+        public void TestCanWriteKO()
+        {
+
+        }
+    }
+}
